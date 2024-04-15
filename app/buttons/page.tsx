02bed46bb18dfcd647fdef7fbbca69f13770fc28
Asishkumar <gouda0682@gmail.com>
2024-04-15 19:49:07 +0530

    Resume at 20.33

---
+++ app/buttons/page.tsx
@@ -0,0 +1,28 @@
+// import {Button} from "@/components/ui/Button";
+
+import { Button } from "@/components/ui/button";
+
+const ButtonsPage = () => {
+  return (
+    <div className="p-4 space-y-4 flex flex-col max-w-[200px]">
+      <Button>
+        Primary
+      </Button>
+      <Button>
+        Primary
+      </Button>
+      <Button>
+        Primary
+      </Button>
+      <Button>
+        Primary
+      </Button>
+      
+    </div>
+
+
+  );
+};
+
+
+export default ButtonsPage;
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,25 +5,19 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-xl text-sm font-bold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 uppercase tracking-wide",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+        default: "bg-white text-black border-slate-200 border-2 border-b-4 active:border-b-2 hover:bg-slate-100 text-slate-500",
+        
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
+        default: "h-11 px-4 py-2",
+        sm: "h-9  px-3",
+        lg: "h-12  px-8",
         icon: "h-10 w-10",
+        rounded: "rounded-full",
       },
     },
     defaultVariants: {
