97d6b3390e8793ce164fc3b338d9526d666e20cb
Asishkumar <gouda0682@gmail.com>
2024-04-18 21:18:59 +0530

    Resume at 56:48 to complete the Authentication

--- app/(Marketing)/header.tsx
+++ app/(Marketing)/header.tsx
@@ -1,8 +1,57 @@
-//write a demo codeimport React from 'react';
+import Image from 'next/image';
+import { Loader } from 'lucide-react';
+import { 
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton
+} from '@clerk/nextjs';
+import { Button } from '@/components/ui/button';
+
+
+
 export const Header = () => {
   return (
     <header className="h-20 w-full border-b-2 border-slate-200 p-4">
-      <h1>Header</h1>
+      <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full ">
+        <div className="pt-8 pl-2 pb-7 flex items-center gap-x-3" >
+          <Image
+            src="/duo_header.svg"
+            alt="Mascot"
+            width={179}
+            height={42}
+          />
+        </div>
+      </div>
+      <div className="absolute top-0 right-0 m-10"> {/* Adjust m-4 according to your preference */}
+        <ClerkLoading>
+          <Loader className='h-5 w-5 text-muted-foreground animate-spin'/>
+        </ClerkLoading>
+      </div>
+      <ClerkLoaded>
+        <SignedIn>
+          <div className='absolute top-[9px] right-[9px] m-2'>
+          <UserButton
+          afterSignOutUrl='/'
+          />
+          </div>
+        </SignedIn>
+        <SignedOut>
+          <SignInButton 
+          mode="modal" 
+          afterSignInUrl="/learn"
+          afterSignUpUrl="/learn"
+          >
+            <Button size="lg" variant="ghost" className='absolute top-[9px] right-[9px] m-2'>
+              Login
+            </Button>
+
+
+          </SignInButton>
+        </SignedOut>
+      </ClerkLoaded>
     </header>
   );
 };
--- app/(Marketing)/page.tsx
+++ app/(Marketing)/page.tsx
@@ -2,8 +2,21 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 export default function Home() {
   return (
-    <div>
-      This is a marketing page
+    <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8  lg-mb-0">
+        <Image
+          src="/duo.svg"
+          fill 
+          alt="Hero"
+          // width={424}
+          // height={424}
+        />
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+          Learn, Practice, and master new languages with Duo !!
+        </h1>
+      </div>
     </div>
 
   );
