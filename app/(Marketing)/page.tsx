6ae41113daf2e98d1915d9750aee08aabb4034cd
Asishkumar <gouda0682@gmail.com>
2024-04-19 12:06:32 +0530

    Authentication Done ✅

--- app/(Marketing)/page.tsx
+++ app/(Marketing)/page.tsx
@@ -1,4 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { 
+    ClerkLoaded,
+    ClerkLoading,
+    SignInButton,
+    SignUpButton,
+    SignedIn,
+    SignedOut,
+    
+  } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Link from "next/link";
 import Image from "next/image";
 export default function Home() {
   return (
@@ -16,6 +27,39 @@ export default function Home() {
         <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
           Learn, Practice, and master new languages with Duo !!
         </h1>
+        <div>
+          <ClerkLoading>
+            <Loader className="h-5 w-5 text-muted-foreground animate-spin"/>
+          </ClerkLoading>
+          <ClerkLoaded>
+              <SignedOut>
+                <SignUpButton 
+                mode="modal"
+                afterSignInUrl="/learn"
+                afterSignUpUrl="/learn">
+                  <Button size="lg" variant="secondary" className="w-full">
+                    Get Started
+                  </Button>
+                </SignUpButton>
+                <SignInButton 
+                mode="modal"
+                afterSignInUrl="/learn"
+                afterSignUpUrl="/learn">
+                  <Button size="lg" variant="primaryOutline" className="w-full">
+                      I already have an account
+                  </Button>
+                </SignInButton>
+              </SignedOut>
+              <SignedIn>
+                <Button size="lg" variant="secondary" className="w-full" asChild>
+                  <Link href="/learn">
+                  Continue Learning
+                  </Link>
+                </Button>
+              </SignedIn>
+            </ClerkLoaded>
+          
+        </div>
       </div>
     </div>
 
