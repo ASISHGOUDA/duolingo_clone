98d262bfa416b866e858a1023a87c50a27e9ad88
Asishkumar <gouda0682@gmail.com>
2024-04-19 17:07:59 +0530

    Footer Done ✅

--- app/(Marketing)/footer.tsx
+++ app/(Marketing)/footer.tsx
@@ -1,7 +1,60 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 export const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+      <Button size="lg" variant="ghost" className="w-full">
+        <Image 
+        src="/hr.svg"
+        alt="croatian"
+        height={32}
+        width={40}
+        className="mr-4 rounded-md"
+        />
+        CROATIAN
+      </Button>
+      <Button size="lg" variant="ghost" className="w-full">
+        <Image 
+        src="/es.svg"
+        alt="Spanish"
+        height={32}
+        width={40}
+        className="mr-4 rounded-md"
+        />
+        SPANISH
+      </Button>
+      <Button size="lg" variant="ghost" className="w-full">
+        <Image 
+        src="/fr.svg"
+        alt="France"
+        height={32}
+        width={40}
+        className="mr-4 rounded-md"
+        />
+        FRENCH
+      </Button>
+      <Button size="lg" variant="ghost" className="w-full">
+        <Image 
+        src="/it.svg"
+        alt="Italy"
+        height={32}
+        width={40}
+        className="mr-4 rounded-md"
+        />
+        ITALIAN
+      </Button>
+      <Button size="lg" variant="ghost" className="w-full">
+        <Image 
+        src="/jp.svg"
+        alt="Japan"
+        height={32}
+        width={40}
+        className="mr-4 rounded-md"
+        />
+        JAPANESE
+      </Button>
+      </div>
     </footer>
   );
 };
--- app/(Marketing)/page.tsx
+++ app/(Marketing)/page.tsx
@@ -11,6 +11,9 @@ import {
 import { Loader } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
+
+
+
 export default function Home() {
   return (
     <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
@@ -25,9 +28,9 @@ export default function Home() {
       </div>
       <div className="flex flex-col items-center gap-y-8">
         <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
-          Learn, Practice, and master new languages with Duo !!
+          Learn, Practice, and master new languages with Duo 🐥 !!
         </h1>
-        <div>
+        <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
           <ClerkLoading>
             <Loader className="h-5 w-5 text-muted-foreground animate-spin"/>
           </ClerkLoading>
