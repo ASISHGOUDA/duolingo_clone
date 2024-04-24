7ed2012a2e14bc80424f5083236dae069b9bed89
Asishkumar <gouda0682@gmail.com>
2024-04-24 18:01:01 +0530

    Minor

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -11,7 +11,7 @@ const MainLayout = ({ children, }: Props) => {
       <MobileHeader />
       <Sidebar className="hidden lg:flex"/>
       <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
-        <div className="h-full bg-red-700">
+        <div className="max-w-[1056px] mx-auto h-full ">
         {children}
         </div>
       </main>
