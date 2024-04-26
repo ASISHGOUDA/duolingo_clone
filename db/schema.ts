9a7d4e04f8a1c178580bd27f8aa404f889d54e29
Asishkumar <gouda0682@gmail.com>
2024-04-26 13:08:37 +0530

    Drizzle & Neon Done✅

---
+++ db/schema.ts
@@ -0,0 +1,9 @@
+import { relations } from "drizzle-orm";
+import { boolean, integer, pgEnum, pgTable, serial, text } from "drizzle-orm/pg-core";
+
+export const courses = pgTable("courses", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull(),
+  imageSrc: text("image_src").notNull(),
+  description: text("description").notNull(),// Remove these afterwards run these two commands npm npm run db:push and save the changes
+});
