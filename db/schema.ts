017fac6d432c4414b9304c4d0e69aa2ca2d82f8b
Asishkumar <gouda0682@gmail.com>
2024-04-29 11:28:54 +0530

    drizzle changes

--- db/schema.ts
+++ db/schema.ts
@@ -5,5 +5,5 @@ export const courses = pgTable("courses", {
   id: serial("id").primaryKey(),
   title: text("title").notNull(),
   imageSrc: text("image_src").notNull(),
-  description: text("description").notNull(),// Remove these afterwards run these two commands npm npm run db:push and save the changes
+  
 });
