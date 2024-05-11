606c241ad913651c05caabae24911303d26f678c
Asishkumar <gouda0682@gmail.com>
2024-05-11 11:30:11 +0530

    Exit Modal Done ✅

--- app/lesson/header.tsx
+++ app/lesson/header.tsx
@@ -1,4 +1,5 @@
 import { Progress } from "@/components/ui/progress";
+import { useExitModal } from "@/store/use-exit-modal";
 
 import { InfinityIcon, X } from "lucide-react";
 import Image from "next/image";
@@ -14,10 +15,12 @@ export const Header = ({
   percentage,
   hasActiveSubscription
 }: Props) => {
+  const { open } = useExitModal();
+
   return (
     <header className="lg:pt-[50px] pt-[20px] px-10 flex gap-x-7 items-center justify-between max-w-[1140px] mx-auto w-full">
       <X
-        onClick={() => {}} //TODO Add onclick exit
+        onClick={open} 
         className="text-slate-500 hover:opacity-75 transition 
         cursor-pointer"
       />
