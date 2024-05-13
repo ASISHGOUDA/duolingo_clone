f1cac2915472a0c943e364e4db6fc1d223d169b6
Asishkumar <gouda0682@gmail.com>
2024-05-13 12:25:13 +0530

    drizzle config.ts  changed updated the version of drizzle-kit 0.21.1

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,8 +4,8 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./db/schema.ts",
   out: "./drizzle",
-  driver:"pg",
+  dialect: "postgresql", // "postgresql" | "mysql"
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
   },
 } satisfies Config;
