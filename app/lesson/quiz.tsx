f2a22422bad2f202aa528a23b6454aad7dede74b
Asishkumar <gouda0682@gmail.com>
2024-05-13 11:22:01 +0530

    Zustand Module added

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -27,6 +27,17 @@ export const Quiz = ({
 }: Props) => {
   const [hearts, setHearts] = useState(initialHearts);
   const [percentage, setPercentage] = useState(initialPercentage);
+  const [challenges] = useState(initialLessonChallenges);
+  const [activeIndex, setActiveIndex] = useState(() => {
+    const uncompletedIndex = challenges.findIndex((challenge) => !challenge.completed);
+    return uncompletedIndex === -1 ? 0 : uncompletedIndex;
+  });
+
+  const challenge = challenges[activeIndex];
+
+  const title = challenge.type === "ASSIST"
+  ? "select the correct meaning" 
+  : challenge.question;
 
   return (
     <>
@@ -35,6 +46,18 @@ export const Quiz = ({
       percentage={percentage}
       hasActiveSubscription={!!userSubscription?.isActive}
       />
+      <div className="flex-1 ">
+          <div className="h-full flex items-center justify-center">
+            <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
+              <h1 className="text-lg lg:text_3xl text-center lg:text-start font-bold text-neutral-700">
+                {title}
+              </h1>
+              <div>
+                {/* Todo: Challenge comoponent */}
+              </div>
+            </div>
+          </div>
+      </div>
     </>
   );
 };
