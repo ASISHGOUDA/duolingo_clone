d470dbcca44c8c3f09a64cb3214eefdaac3a2d5a
Asishkumar <gouda0682@gmail.com>
2024-05-13 15:22:52 +0530

    resume @ 6.08.25

--- app/lesson/card.tsx
+++ app/lesson/card.tsx
@@ -1,4 +1,7 @@
+import { cn } from "@/lib/utils";
 import { challenges } from "@/db/schema";
+import Image from "next/image";
+
 
 type Props = {
   id: number;
@@ -30,8 +33,26 @@ export const Card = ({
   type,
 }: Props) => {
   return (
-    <div>
-      card
+    <div
+    onClick={() => {}}
+    className={cn(
+      "h-full border-2 rounded-xl border-b-4 hover:bg-black/5 p-4 lg:p-6 cursor-pointer acticve:border-b-2",
+      selected && "border-sky-300 bg-sky-100 hover:bg-sky-100",
+      selected && status === "correct" && "border-green-300 bg-green-100 hover:bg-green-100",
+      selected && status === "wrong" && "border-rose-300 bg-rose-100 hover:bg-rose-100",
+      disabled && "pointer-events-none hover:bg-white",
+      type === "ASSIST" && "lg:p-3 w-full",
+    )}
+    >
+      {imageSrc && (
+        <div className="relative aspect-square mb-4 max-h-[80px] lg:max-h-[150px] w-full">
+          <Image
+            src={imageSrc}
+            alt={text}
+            fill
+          />
+        </div>
+      )}
     </div>
   );
 };
